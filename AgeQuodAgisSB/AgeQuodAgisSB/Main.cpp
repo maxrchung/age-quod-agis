908eccb283873c0fec821e2e5c1755c077942908
maxrchung <maxrchung@gmail.com>
2016-03-30 11:17:11 -0700

    Starting with snowflake generation; linked SFML; probably going to make custom 1/6 designs in Photoshop and have SFML mirror the other 5 parts for me

--- AgeQuodAgisSB/AgeQuodAgisSB/Main.cpp
+++ AgeQuodAgisSB/AgeQuodAgisSB/Main.cpp
@@ -13,13 +13,25 @@ int main() {
 	std::string storyboardPath = beatmapDirectory + beatmapTitle + ".osb";
 
 	// Calculate BPM related values
+	float bpm = 140.0f;
+	float mpb = 1 / bpm;
+	float spb = mpb * 60;
+	// ~429 ms per beat
+	float mspb = 1000 * spb;
+
+	// Midpoint of screen
+	Vector2 midpoint(320, 240);
+	Vector2 centerpieceSize(200, 200);
+	Sprite* centerpiece = new Sprite("centerpiece.png", midpoint, centerpieceSize);
 
 	// Parse in hit objects into notes and holds
 	Beatmap::Instance()->ParseHitObjects(beatmapPath);
 
 	// Deal with notes
 	for (auto note : Beatmap::Instance()->notes) {
+		if (note->lane == 1) {
 
+		}
 	}
 
 	// Deal with holds
